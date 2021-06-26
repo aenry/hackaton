216215a2c2b1ebb1ca1c7bbd26c7fd035a3cff32
dnaenry <dnaenry@gmail.com>
2021-06-26 17:02:12 -0400

    templates-editado

--- env/hackathon/peoplemarket/views.py
+++ env/hackathon/peoplemarket/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def home(request):
     return render(request,'peoplemarket/home.html')
 def empresadata(request):
+    if request.method == 'POST':
+        return redirect('/agregarproducto')
     return render(request, 'peoplemarket/empresadata.html')
 
 def agregarproducto(request):
