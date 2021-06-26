5add9cac622e40763e7a73f42b8aa5cbc950c669
robinson <robi.tobar@duocuc.cl>
2021-06-26 14:16:22 -0400

    modelos y paginas adm

--- env/hackathon/peoplemarket/admin.py
+++ env/hackathon/peoplemarket/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.db import models 
+from .models import *
+
 
 # Register your models here.
+class admRubro(admin.ModelAdmin):
+    list_display = ['id', 'descripcion']
+    class Meta:
+        model=Rubro
+admin.site.register(Rubro,admRubro)
+
+class admCategoria(admin.ModelAdmin):
+    list_display = ['id', 'descripcion']
+    class Meta:
+        model=Categoria
+admin.site.register(Categoria,admCategoria)
+
+class admTienda(admin.ModelAdmin):
+    list_display = ['rut', 'nombre', 'direccion', 'rubro']
+    class Meta:
+        model=Tienda
+admin.site.register(Tienda,admTienda)
+
+class admProducto(admin.ModelAdmin):
+    list_display = ['nombre', 'categoria', 'precio', 'stock']
+    class Meta:
+        model=Producto
+admin.site.register(Producto,admProducto)
