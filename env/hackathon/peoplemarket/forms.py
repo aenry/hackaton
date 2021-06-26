be9b3356999af48332d79d2785ac1b20515da2d1
robinson <robi.tobar@duocuc.cl>
2021-06-26 14:52:51 -0400

    Actualizacion comuna

---
+++ env/hackathon/peoplemarket/forms.py
@@ -0,0 +1,3 @@
+from django import forms
+from django.forms import fields
+from peoplemarket.models import * 
--- env/hackathon/peoplemarket/models.py
+++ env/hackathon/peoplemarket/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Region(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length=25)
+    def __str__(self):
+        return str(self.nombre)
+
+class Comuna(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length=25)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.nombre)
+
 class Rubro(models.Model):
     id = models.AutoField(primary_key = True)
     descripcion = models.CharField(max_length=25)
@@ -17,6 +30,8 @@ class Tienda(models.Model):
     rut = models.CharField(max_length=9, primary_key=True)
     nombre = models.CharField(max_length=25)
     direccion = models.CharField(max_length=25)
+    comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
+    logo = models.CharField(max_length=255)
     rubro = models.ForeignKey(Rubro, on_delete=models.CASCADE)
     def __str__(self):
         return str(self.nombre)
@@ -24,6 +39,7 @@ class Tienda(models.Model):
 class Producto(models.Model):
     id = models.AutoField(primary_key = True)
     nombre = models.CharField(max_length=25)
+    logo = models.CharField(max_length=255)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     precio = models.PositiveIntegerField()
     stock = models.PositiveIntegerField()
